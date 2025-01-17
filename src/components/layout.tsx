d12451dfba9b19da048b987460c5ed3ef5ffebdf
ToastedToast <hey@toasted.dev>
2025-01-17 22:34:32 +0800

    chore(navbar): replace <a> with <Link>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export function Layout() {
   return (
     <>
       <header className="px-4 py-3 border-b border-zinc-600 flex justify-between">
-        <a href="/" className="flex items-center gap-1">
+        <Link to="/" className="flex items-center gap-1">
           <h1 className="text-xl font-semibold">CountStream</h1>
           <sub className="text-purple-500">by SCTools</sub>
-        </a>
+        </Link>
       </header>
       <main>
         <Outlet />
