008e577669da4fa1786d8bd64257f5ebbcce244b
ToastedToast <hey@toasted.dev>
2025-02-07 16:34:32 +0800

    chore(queries): clear data on navigate

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,21 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { Info } from "lucide-react";
 import { Link, Outlet } from "react-router";
 
 export function Layout() {
+  const queryClient = useQueryClient();
+
   return (
     <>
       <div className="border-b border-zinc-600">
         <header className="mx-auto flex w-full max-w-7xl justify-between px-4 py-3">
-          <Link to="/" className="flex items-center gap-1">
+          <Link
+            to="/"
+            onClick={() => {
+              queryClient.clear();
+            }}
+            className="flex items-center gap-1"
+          >
             <h1 className="text-xl font-semibold">CountStream</h1>
             <sub className="text-purple-500">by SCTools</sub>
           </Link>
