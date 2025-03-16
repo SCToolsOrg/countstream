56401b510436e198771726173a72694786232fef
ToastedToast <hey@toasted.dev>
2025-03-16 19:29:19 +0800

    feat: init nextjs project

--- src-old/hooks/use-user.ts
+++ src-old/hooks/use-user.ts
@@ -1,7 +1,5 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { map } from "nanostores";
-import { useEffect, useMemo, useState } from "react";
-import { useLocation } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 export interface API {
   id: string;
