222ddb6dc47b1a29fc2ed21c34ae36384ee62d7d
ToastedToast <hey@toasted.dev>
2025-06-04 17:42:14 +0800

    feat: add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,17 @@
   let { children } = $props();
 </script>
 
-{@render children()}
+<div class="bg-accent">
+  <header class="mx-auto flex w-full max-w-7xl justify-between px-4 py-3">
+    <a href="/" class="flex items-center gap-1">
+      <h1 class="text-xl font-semibold">
+        count<span class="text-primary">stream</span>
+      </h1>
+      <sub>by SCTools</sub>
+    </a>
+  </header>
+</div>
+
+<main>
+  {@render children()}
+</main>
