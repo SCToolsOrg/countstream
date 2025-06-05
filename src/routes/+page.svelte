f4fbee49c45eb482de39e1a298479dfee99e6087
ToastedToast <hey@toasted.dev>
2025-06-05 10:25:27 +0800

    feat: homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<script lang="ts">
+  import { Card } from "$lib/components/ui/card";
+  import { counts, type Count } from "$lib/counts";
+
+  function convertPlatformToName(platform: string) {
+    switch (platform) {
+      case "youtube":
+        return "YouTube";
+      case "tiktok":
+        return "TikTok";
+      default:
+        return platform.at(0)!.toUpperCase() + platform.slice(1).toLowerCase();
+    }
+  }
+
+  const countsByPlatform = Object.values(
+    counts.reduce(
+      (acc, obj) => {
+        acc[obj.platform] ??= {
+          name: obj.platform,
+          icon: obj.icon,
+          color: obj.color,
+          counts: [],
+        };
+        acc[obj.platform].counts.push(obj);
+        return acc;
+      },
+      {} as Record<
+        string,
+        {
+          name: string;
+          icon: string;
+          color: string;
+          counts: Count[];
+        }
+      >
+    )
+  );
+</script>
+
+<div
+  class="grid grid-cols-1 justify-center gap-2 md:grid-cols-2 lg:grid-cols-3"
+>
+  {#each countsByPlatform as platform (platform.name)}
+    <Card
+      class="flex flex-col items-center justify-center gap-1 overflow-hidden text-center"
+    >
+      <img
+        src={platform.icon}
+        alt={platform.name}
+        width={64}
+        height={64}
+        class="z-50 h-16 object-contain"
+      />
+      <h1 class="text-xl font-bold">
+        {convertPlatformToName(platform.name)}
+      </h1>
+      {#each platform.counts as count (count.type)}
+        <a
+          href={`${platform.name}/${count.type}`}
+          class="hover:text-primary transition-colors"
+        >
+          {count.name}
+        </a>
+      {/each}
+    </Card>
+  {/each}
+</div>
