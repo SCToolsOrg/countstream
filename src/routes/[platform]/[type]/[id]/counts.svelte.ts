f4b7d13eeee9be7a94f83bb5177f8880d18d93d2
ToastedToast <hey@toasted.dev>
2026-01-15 21:40:45 +0800

    disable studio for now
    been having a lot of problems, can't deal with it rn

--- src/routes/[platform]/[type]/[id]/counts.svelte.ts
+++ src/routes/[platform]/[type]/[id]/counts.svelte.ts
@@ -17,97 +17,13 @@ export function useCounts(
   run?: (counts: number[]) => void
 ) {
   const counts = writable<number[]>([]);
-  const isStudio = writable<boolean | undefined>(undefined);
   const goalCount = $derived(
     parseInt(page.url.searchParams.get("goal-count") ?? "0")
   );
-  let studioInterval = $state.raw<ReturnType<typeof setInterval>>();
-
-  const check = async () => {
-    try {
-      const res = await fetch(
-        `https://api-v2.nextcounts.com/api/youtube/channel/${id}`
-      );
-      const data = await res.json();
-      if (!data.success) return;
-      isStudio.set(data.verifiedSubCount);
-    } catch {
-      // do nothing
-    }
-  };
-
-  $effect(() => {
-    if (count.platform === "youtube" && count.type === "channel") {
-      if (id === "UCX6OQ3DkcsbYNE6H8uQQuVA" || jsalStatsChannels.includes(id)) {
-        isStudio.set(true);
-      } else {
-        check();
-      }
-    }
-  });
-
-  let studio = $state.raw<boolean | undefined>(undefined);
-  isStudio.subscribe((isStudio) => {
-    studio = isStudio;
-  });
 
   $effect(() => {
     const update = async () => {
-      if (
-        count.platform === "youtube" &&
-        count.type === "channel" &&
-        typeof studio === "undefined"
-      )
-        return;
-
-      let newCounts: number[];
-      if (
-        count.platform === "youtube" &&
-        count.type === "channel" &&
-        studio &&
-        id !== "UCX6OQ3DkcsbYNE6H8uQQuVA"
-      ) {
-        if (studioInterval) {
-          clearInterval(studioInterval);
-          studioInterval = undefined;
-        }
-
-        if (jsalStatsChannels.includes(id)) {
-          const [subCounts, data] = await Promise.all([
-            fetch("https://studio.jsalstats.xyz/subcount"),
-            fetch(`https://backend.mixerno.space/api/youtube/estv3/${id}`),
-          ]).then((responses) =>
-            Promise.all(responses.map((res) => res.json()))
-          );
-
-          newCounts = [
-            subCounts[id],
-            data.items[0].statistics.subscriberCountAPI,
-            data.items[0].statistics.viewCountAPI,
-            data.items[0].statistics.videoCount,
-          ];
-        } else {
-          const res = await fetch(
-            `https://api-v2.nextcounts.com/api/youtube/channel/${id}`
-          );
-          const data = await res.json();
-          isStudio.set(data.verifiedSubCount);
-
-          const abbreviateNumber = (num: number) =>
-            num.toString().slice(0, 3) +
-            new Array(num.toString().length - 3).fill("0").join("");
-          newCounts = [
-            data.subcount,
-            abbreviateNumber(data.subcount),
-            data.viewcount,
-            data.videos,
-          ];
-        }
-      } else {
-        newCounts = await count.getCounts(id);
-        studioInterval = setInterval(check, 5 * 60 * 1000);
-      }
-
+      let newCounts = await count.getCounts(id);
       counts.set([...newCounts, calculateGoal(newCounts[goalCount])]);
       run?.(newCounts);
     };
